ed6719559f402317d8aff6900e28889ce139126e
georgieva17r <r.georgieva2021@pmg-pernik.com>
2026-01-12 09:05:23 +0200

    pt.5

---
+++ WebShopApp.Core/Contracts/IOrderService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShopApp.Infrastructure.Data.Domain;
+
+namespace WebShopApp.Core.Contracts
+{
+    public interface IOrderService
+    {
+        bool Create(int productId, string userId, int quantity);
+
+        List<Order> GetOrders();
+        List<Order> GetOrdersByProductId(string useId);
+        Order GetOrderById(int orderId);
+        bool RemoveById(int orderId);
+        bool Update (int orderId, int productId, string userId, int quantity);
+    }
+}
---
+++ WebShopApp.Core/Services/OrderService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShopApp.Core.Contracts;
+using WebShopApp.Infrastructure.Data.Domain;
+
+namespace WebShopApp.Core.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IProductService _productService;  
+        
+        public OrderService(AplicationDbContext context, IProductService productService)
+        {
+            _context = context;
+            _productService = productService;
+        }
+        public bool Create(int productId, string userId, int quantity)
+        {
+            var product = this._context.Products.SingleOrDefault(x => x.Id == productId);
+
+            if (product == null)
+            {
+                return false;
+            }
+            Order item = new Order
+            {
+                OrderDate = DateTime.Now,
+                ProductId = productId,
+                UserId = userId,
+                Quantity = quantity,
+                Price = product.Price,
+                Discount = product.Discount
+            };
+            product.Quantity -= quantity;
+            this._context.Products.Update(product);
+            this._context.Orders.Add(item);
+            return _context.SaveChanges() !=0;
+        }
+
+        public List<Order> GetOrders()
+        {
+            return _context.Orders.OrderByDescending(x => x.OrderDate).ToList();
+        }
+    }
+}
